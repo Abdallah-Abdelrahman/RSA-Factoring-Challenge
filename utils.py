bdb0c8ddd969c39a992ba414cd1ae8caea2f6fec
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-26 20:48:38 +0300

    Try `is_prime` function

---
+++ utils.py
@@ -0,0 +1,21 @@
+
+"""This module defines utils functions"""
+
+
+def sum_digits(n):
+    """Sum digits of a positive int ``n``"""
+    if n < 10:
+        return n
+    return n % 10 + sum_digits(n // 10)
+
+def is_prime(num):
+    """Check if ``num`` is prime"""
+    if num == 2:
+        return True
+    if (num % 2 == 0):
+        return False
+    if num > 9 and num % 5 == 0:
+        return False
+    if sum_digits(num) % 3 == 0:
+        return False
+    return True
